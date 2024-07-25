64d5376d251483c06ef390d96972ba95b9405783
Javier Andreu <andreujavier99@gmail.com>
2024-07-25 13:35:00 +0200

    bonus + fds folder

--- b
+++
@@ -1,8 +0,0 @@
-linea del b
-linea del b 2
-linea del b 3
-linea del b 4
-linea del b 5
-linea del b 6
-se acabo B
-.
---
+++ get_next_line_bonus.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antandre <antandre@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 15:29:42 by antandre          #+#    #+#             */
+/*   Updated: 2024/07/25 10:56:50 by antandre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*saved[1024];
+	int			readed;
+	char		*buffer;
+
+	if (saved[fd] && ft_isnewline(saved[fd]) > 0)
+		return (ft_strtrim_jump(&saved[fd]));
+	else
+	{
+		buffer = NULL;
+		while (!ft_isnewline(saved[fd]))
+		{
+			buffer = malloc(BUFFER_SIZE * sizeof(char));
+			if (!buffer)
+				return (NULL);
+			readed = read(fd, buffer, BUFFER_SIZE);
+			if (readed > 0)
+				readed = ft_strncat(&saved[fd], buffer, readed);
+			free(buffer);
+			buffer = NULL;
+			if (!readed)
+				return (NULL);
+		}
+		return (ft_strtrim_jump(&saved[fd]));
+	}
+	return (NULL);
+}
